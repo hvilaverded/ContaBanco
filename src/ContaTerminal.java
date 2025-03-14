d50a9193e2915f27205109aecb07d1ee81b502ac
Heitor <vilaverde88@gmail.com>
2025-03-14 07:11:20 -0300

    commit do programa

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+   
+   public static void main(String[] args) {
+   // Declaração de variáveis
     int numero;
     String agencia;
     String nomeCliente;
-    double saldo;
+    double saldo = 0.0;
 
+    // conhecer e importar a classe Scanner
+    Scanner conta = new Scanner(System.in);
+   
+    
+    System.out.println("Digite o número da conta: "); //Exibir mensagem para o usuário
+    numero = conta.nextInt();                           //Ler o número da conta
+
+    System.out.println("Digite o numero da Agencia: ");
+    agencia = conta.next();
+
+    System.out.println("Digite o nome do cliente: ");
+    nomeCliente = conta.next();
+
+    System.out.println("Digite o valor depositado: ");
+    saldo = conta.nextDouble();
+
+    //Exibir mensagem conta criada
+    System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+     + agencia + " conta: " + numero +  " e seu saldo de R$" + saldo +" já está disponível para saque.");
+
+   }
+   
+    
 }
